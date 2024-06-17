798590aeb6ee5165490dcff26f003042662cfe85
Verma Adarshkumar Dharmendrakumar <111121772+adarsh7123@users.noreply.github.com>
2024-06-17 08:34:25 +0530

    Time: 5 ms (34.5%), Space: 7.1 MB (61.35%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -1,12 +1,14 @@
-#include <bits/stdc++.h>
 class Solution {
 public:
     bool judgeSquareSum(int c) {
-        for (long a = 0; a * a <= c; a++) {
-            double b = sqrt(c - a * a);
-            if (b == (int)b)
-                return true;
+        for (long a = 0; a * a <= c;
+             a++) { // Iterate through all possible values of `a`
+            double b =
+                sqrt(c - a * a); // Compute `b` as the square root of `c - a^2`
+            if (b == (int)b) {   // Check if `b` is an integer
+                return true;     // If `b` is an integer, return true
+            }
         }
-        return false;
+        return false; // If no such pair `(a, b)` is found, return false
     }
 };
