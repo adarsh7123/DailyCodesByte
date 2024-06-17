bca7e1175370804f4209960c1bf6cb6754461db9
Verma Adarshkumar Dharmendrakumar <111121772+adarsh7123@users.noreply.github.com>
2024-06-17 08:33:37 +0530

    Time: 2 ms (70.95%), Space: 7 MB (91.95%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        for (long a = 0; a * a <= c; a++) {
+            double b = sqrt(c - a * a);
+            if (b == (int)b)
+                return true;
+        }
+        return false;
+    }
+};
