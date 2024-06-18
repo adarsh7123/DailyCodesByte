0379571e490f5a75490f88b63ec4a073dbe2dcce
Verma Adarshkumar Dharmendrakumar <111121772+adarsh7123@users.noreply.github.com>
2024-06-18 11:13:18 +0530

    Time: 9 ms (11.18%), Space: 11.4 MB (32.71%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -2,14 +2,20 @@ class Solution {
 public:
     int solve(int i, int j, vector<vector<int>>& triangle,
               vector<vector<int>>& dp) {
+                //base case
         if (i >= triangle.size() || j >= triangle[i].size())
             return 0;
 
+//if dp previously find
         if (dp[i][j] != -1)
             return dp[i][j];
 
+
+//find triangle pathcost [i+1][j] and [i+1][j+1]
         int op1 = triangle[i][j] + solve(i + 1, j, triangle, dp);
         int op2 = triangle[i][j] + solve(i + 1, j + 1, triangle, dp);
+
+        //return min
         return dp[i][j] = min(op1, op2);
     }
     int minimumTotal(vector<vector<int>>& triangle) {
