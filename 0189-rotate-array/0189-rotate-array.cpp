21f4e319ef4249c049beb78bf050b0c5e7ab7c45
Verma Adarshkumar Dharmendrakumar <111121772+adarsh7123@users.noreply.github.com>
2024-06-23 10:26:45 +0530

    Time: 19 ms (70.55%), Space: 27.4 MB (39.37%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -4,12 +4,16 @@ public:
         int n = nums.size();
         k = k % n;
 
-        vector<int> b;
-        for (int i = n - k; i < n; i++)
-            b.push_back(nums[i]);
-        for (int i = 0; i < n - k; i++)
-            b.push_back(nums[i]);
-        for (int i = 0; i < n; i++)
-            nums[i] = b[i];
+        // vector<int> b;
+        // for (int i = n - k; i < n; i++)
+        //     b.push_back(nums[i]);
+        // for (int i = 0; i < n - k; i++)
+        //     b.push_back(nums[i]);
+        // for (int i = 0; i < n; i++)
+        //     nums[i] = b[i];
+
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin() + k);
+        reverse(nums.begin() + k, nums.end());
     }
 };
