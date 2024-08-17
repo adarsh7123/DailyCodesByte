53bc37a52522120cd400f4b7910961e19d5cd05c
Verma Adarshkumar Dharmendrakumar <111121772+adarsh7123@users.noreply.github.com>
2024-08-17 11:46:14 +0530

    Time: 18 ms (9.93%), Space: 22.3 MB (19.65%) - LeetHub

--- 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.cpp
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.cpp
@@ -1,18 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
 class Solution {
 public:
-    TreeNode* sortedArrayToBST(vector<int>& nums) {
-        return sortedArrayToBSTHelper(nums, 0, nums.size() - 1);
+    TreeNode* helper(vector<int>& nums, int low, int high) {
+        if (low > high)
+            return NULL;
+        int mid = low + (high - low) / 2;
+
+        TreeNode* root = new TreeNode(nums[mid]);
+
+        root->left = helper(nums, low, mid - 1);
+
+        root->right = helper(nums, mid+1, high);
+        return root;
     }
 
-private:
-    TreeNode* sortedArrayToBSTHelper(const vector<int>& nums, int start, int end) {
-        if (start > end) {
-            return nullptr;
-        }
-        int mid = start + (end - start) / 2;
-        TreeNode* root = new TreeNode(nums[mid]);
-        root->left = sortedArrayToBSTHelper(nums, start, mid - 1);
-        root->right = sortedArrayToBSTHelper(nums, mid + 1, end);
-        return root;
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        int n = nums.size();
+        return helper(nums, 0, n - 1);
     }
 };
